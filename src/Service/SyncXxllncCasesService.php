f095076daeca80de042ac468a60ca6cd05b5f08b
Barry Brands <barrybrands02@hotmail.com>
2023-08-25 10:31:25 +0200

    logs added

--- src/Service/SyncXxllncCasesService.php
+++ src/Service/SyncXxllncCasesService.php
@@ -12,6 +12,7 @@ use Psr\Cache\CacheException;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Psr\Log\LoggerInterface;
 
 /**
  * Service responsible for synchronizing xxllnc cases to woo objects.
@@ -56,6 +57,11 @@ class SyncXxllncCasesService
     private ?SymfonyStyle $style = null;
 
     /**
+     * @var LoggerInterface $logger.
+     */
+    private LoggerInterface $logger;
+
+    /**
      * @var array
      */
     private array $data;
@@ -74,20 +80,22 @@ class SyncXxllncCasesService
      * @param SynchronizationService $syncService
      * @param EntityManagerInterface $entityManager
      * @param MappingService         $mappingService
+     * @param LoggerInterface        $pluginLogger
      */
     public function __construct(
         GatewayResourceService $resourceService,
         CallService $callService,
         SynchronizationService $syncService,
         EntityManagerInterface $entityManager,
-        MappingService $mappingService
+        MappingService $mappingService,
+        LoggerInterface $pluginLogger
     ) {
         $this->resourceService = $resourceService;
         $this->callService     = $callService;
         $this->syncService     = $syncService;
         $this->entityManager   = $entityManager;
         $this->mappingService  = $mappingService;
-
+        $this->logger          = $pluginLogger;
     }//end __construct()
 
 
@@ -125,42 +133,49 @@ class SyncXxllncCasesService
         $this->configuration = $configuration;
 
         isset($this->style) === true && $this->style->success('SyncXxllncCasesService triggered');
+        $this->logger->info('SyncXxllncCasesService triggered');
 
         if (isset($this->configuration['source']) === false) {
-            isset($this->style) === true && $this->style->error('No source configured on this action, ending action.');
+            isset($this->style) === true && $this->style->error('No source configured on this action, ending syncXxllncCasesHandler');
+            $this->logger->error('No source configured on this action, ending syncXxllncCasesHandler');
 
             return [];
         }
 
         if (isset($this->configuration['oidn']) === false) {
-            isset($this->style) === true && $this->style->error('No oidn configured on this action, ending action.');
+            isset($this->style) === true && $this->style->error('No oidn configured on this action, ending syncXxllncCasesHandler');
+            $this->logger->error('No oidn configured on this action, ending syncXxllncCasesHandler');
 
             return [];
         }
 
         $source = $this->resourceService->getSource($this->configuration['source'], 'common-gateway/pdd-bundle');
         if ($source === null) {
-            isset($this->style) === true && $this->style->error("{$this->configuration['source']} not found.");
+            isset($this->style) === true && $this->style->error("{$this->configuration['source']} not found, ending syncXxllncCasesHandler");
+            $this->logger->error("{$this->configuration['source']} not found, ending syncXxllncCasesHandler");
             return [];
         }
 
         $schemaRef = 'https://commongateway.nl/pdd.openWOO.schema.json';
         $schema    = $this->resourceService->getSchema($schemaRef, 'common-gateway/pdd-bundle');
         if ($schema === null) {
-            isset($this->style) === true && $this->style->error("$schemaRef not found.");
+            isset($this->style) === true && $this->style->error("$schemaRef not found, ending syncXxllncCasesHandler");
+            $this->logger->error("$schemaRef not found, ending syncXxllncCasesHandlerr");
             return [];
         }
 
         $mappingRef = 'https://commongateway.nl/mapping/pdd.xxllncCaseToWoo.schema.json';
         $mapping    = $this->resourceService->getMapping($mappingRef, 'common-gateway/pdd-bundle');
         if ($mapping === null) {
-            isset($this->style) === true && $this->style->error("$mappingRef not found.");
+            isset($this->style) === true && $this->style->error("$mappingRef not found, ending syncXxllncCasesHandler");
+            $this->logger->error("$mappingRef not found, ending syncXxllncCasesHandlerr");
             return [];
         }
 
         $sourceConfig = $source->getConfiguration();
 
         isset($this->style) === true && $this->style->info("Fetching cases from {$source->getLocation()}");
+        $this->logger->info("Fetching cases from {$source->getLocation()}");
 
         $response        = $this->callService->call($source, '', 'GET', $sourceConfig);
         $decodedResponse = $this->callService->decodeResponse($source, $response);
@@ -183,7 +198,8 @@ class SyncXxllncCasesService
 
         $this->data['response'] = new Response(json_encode($responseItems), 200);
 
-        isset($this->style) === true && $this->style->success("Synchronized cases to woo objects.");
+        isset($this->style) === true && $this->style->success("Synchronized cases to woo objects for " . $source->getName());
+        $this->logger->info("Synchronized cases to woo objects for " . $source->getName());
 
         return $this->data;
 
