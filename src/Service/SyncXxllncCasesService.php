de27237ddf5606fb57a8fa2ad88c117ea2eb0212
Barry Brands <barrybrands02@hotmail.com>
2023-09-14 12:44:51 +0200

    fix sync

--- src/Service/SyncXxllncCasesService.php
+++ src/Service/SyncXxllncCasesService.php
@@ -14,7 +14,7 @@ use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Psr\Log\LoggerInterface;
-use App\Entity\Entity as Schema;
+use App\Entity\Gateway as Source;
 
 /**
  * Service responsible for synchronizing xxllnc cases to woo objects.
@@ -131,32 +131,36 @@ class SyncXxllncCasesService
      * Checks if existing objects still exist in the source, if not deletes them.
      *
      * @param array  $idsSynced ID's from objects we just synced from the source.
-     * @param Schema $schame    These objects belong to.
+     * @param Source $source These objects belong to.
+     * @param string $schemaRef These objects belong to.
      *
-     * @return void Returns nothing.
+     * @return int Count of deleted objects.
      */
-    private function deleteNonExistingObjects(array $idsSynced, Schema $schema): void
+    private function deleteNonExistingObjects(array $idsSynced, Source $source, string $schemaRef): int
     {
         // Get all existing sourceIds.
-        $schema = $this->entityManager->find('App:Entity', $schema->getId()->toString());
-        foreach ($schema->getObjectEntities() as $objectEntity) {
-            $synchronizations = $objectEntity->getSynchronizations();
-            if (isset($synchronizations[0]) === true && $synchronizations[0]->getSourceId() !== null) {
-                $existingSourceIds[] = $synchronizations[0]->getSourceId();
-                $existingObjects[]   = $objectEntity;
+        $source = $this->entityManager->find('App:Gateway', $source->getId()->toString());
+        foreach ($source->getSynchronizations() as $synchronization) {
+            if ($synchronization->getEntity()->getReference() === $schemaRef && $synchronization->getSourceId() !== null) {
+                $existingSourceIds[] = $synchronization->getSourceId();
+                $existingObjects[]   = $synchronization->getObject();
             }
-        }
+        }    
 
         // Check if existing sourceIds are in the array of new synced sourceIds.
         $objectIdsToDelete = array_diff($existingSourceIds, $idsSynced);
 
         // If not it means the object does not exist in the source anymore and should be deleted here.
+        $deletedObjectsCount = 0;
         foreach ($objectIdsToDelete as $key => $id) {
             $this->logger->info("Object $id does not exist at the source, deleting.");
             $this->entityManager->remove($existingObjects[$key]);
+            $deletedObjectsCount++;
         }
 
         $this->entityManager->flush();
+
+        return $deletedObjectsCount;
 
     }//end deleteNonExistingObjects()
 
@@ -237,6 +241,9 @@ class SyncXxllncCasesService
                 $this->logger->error("SyncXxllncCases validation errors: $validationErrors");
                 continue;
             }
+            if (isset($result['Categorie']) === false) {
+                continue;
+            }
 
             $object = $hydrationService->searchAndReplaceSynchronizations(
                 $result,
@@ -254,12 +261,12 @@ class SyncXxllncCasesService
             $responseItems[] = $object;
         }//end foreach
 
-        $this->deleteNonExistingObjects($idsSynced, $schema);
+        $deletedObjectsCount = $this->deleteNonExistingObjects($idsSynced, $source, $schemaRef);
 
         $this->data['response'] = new Response(json_encode($responseItems), 200);
 
         $countItems = count($responseItems);
-        $logMessage = "Synchronized $countItems cases to woo objects for ".$source->getName();
+        $logMessage = "Synchronized $countItems cases to woo objects for ".$source->getName() ." and deleted $deletedObjectsCount objects";
         isset($this->style) === true && $this->style->success($logMessage);
         $this->logger->info($logMessage);
 
