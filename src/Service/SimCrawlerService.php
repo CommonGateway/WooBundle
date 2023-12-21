4a5ee47775e1e6687c41f745eb8d18822a7ad1ea
Robert Zondervan <robert@conduction.nl>
2023-12-21 13:48:51 +0100

    Add docblocks

--- src/Service/SimCrawlerService.php
+++ src/Service/SimCrawlerService.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Service to crawl SIMsite websites and store pages as WOO publications.
+ *
+ * This service provides a crawler to store pages on SIMsite websites as WOO publications.
+ *
+ * @Author Ruben van der Linde <ruben@conduction.nl>, Robert Zondervan <robert@conduction.nl>, Sarai Misidjan <sarai@conduction.nl>, Barry Brands <barry@conduction.nl>, Wilco Louwerse <wilco@conduction.nl>
+ *
+ * @license EUPL <https://github.com/ConductionNL/contactcatalogus/blob/master/LICENSE.md>
+ *
+ * @category Service
+ */
 namespace CommonGateway\WOOBundle\Service;
 
 use App\Entity\ObjectEntity;
@@ -12,29 +22,76 @@ use Doctrine\ORM\EntityManagerInterface;
 class SimCrawlerService
 {
 
+    /**
+     * The gateway resource service.
+     *
+     * @var GatewayResourceService
+     */
     private GatewayResourceService $resourceService;
 
+    /**
+     * The call service.
+     *
+     * @var CallService
+     */
     private CallService $callService;
 
+    /**
+     * The mapping service.
+     *
+     * @var MappingService
+     */
     private MappingService $mappingService;
 
+    /**
+     * The entity manager.
+     *
+     * @var EntityManagerInterface
+     */
     private EntityManagerInterface $entityManager;
 
+    /**
+     * The synchronization service.
+     *
+     * @var SynchronizationService
+     */
+    private SynchronizationService $synchronizationService;
 
+
+    /**
+     * Service constructor.
+     *
+     * @param CallService $callService The call service.
+     * @param EntityManagerInterface $entityManager The entity manager.
+     * @param GatewayResourceService $resourceService The resource service.
+     * @param MappingService $mappingService The mapping service.
+     * @param SynchronizationService $synchronizationService The synchronization service.
+     */
     public function __construct(
         CallService $callService,
         EntityManagerInterface $entityManager,
         GatewayResourceService $resourceService,
-        MappingService $mappingService
+        MappingService $mappingService,
+        SynchronizationService $synchronizationService
     ) {
         $this->callService     = $callService;
         $this->entityManager   = $entityManager;
         $this->resourceService = $resourceService;
         $this->mappingService  = $mappingService;
+        $this->synchronizationService = $synchronizationService;
+
 
     }//end __construct()
 
 
+    /**
+     * Retrieve a sitemap.xml from a source, parse it into a list of paths, fetch metadata and map it into publications.
+     *
+     * @param array $data The data retrieved by the action.
+     * @param array $configuration The configuration of the action.
+     *
+     * @return array The resulting data array.
+     */
     public function SimSiteHandler(array $data, array $configuration): array
     {
         $source         = $this->resourceService->getSource($configuration['source'], 'common-gateway/woo-bundle');
