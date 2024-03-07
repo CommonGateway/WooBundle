21b124ad38252211b46a97b3dd0315385f46fb8e
Barry Brands <barrybrands02@hotmail.com>
2024-03-07 16:40:34 +0100

    removed some unnecessary stuff

--- src/Service/SyncOpenWooService.php
+++ src/Service/SyncOpenWooService.php
@@ -299,7 +299,7 @@ class SyncOpenWooService
                 'naam' => $this->configuration['organisatie'],
             ],
             'categorie'   => $categorie,
-            'autoPublish' => $this->configuration['autoPublish'] ?? null,
+            'autoPublish' => $this->configuration['autoPublish'] ?? true,
         ];
 
         $idsSynced        = [];
